55e0d719eccefe632677045a2e1be559a4cefed5
Tomedaro <daniil.tomenko.r@gmail.com>
2026-06-08 15:17:14 +0200

    refactor(kitty): switch to local amber-cathode theme
    Move color definitions from inline kitty settings into a dedicated
    custom theme file (amber-cathode.conf). This keeps generated config
    clean and makes theme swapping easier.

--- modules/programs/terminal/kitty/default.nix
+++ modules/programs/terminal/kitty/default.nix
@@ -1,19 +1,32 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   home-manager.sharedModules = [
     (_: {
+      xdg.configFile."kitty/themes/amber-cathode.conf".source =
+        ./themes/amber-cathode.conf;
+
       programs.kitty = {
         enable = true;
+
         font = {
           size = 14.0;
           name = "monospace";
         };
-        themeFile = "gruvbox-dark-hard";
+
+        # Do not use themeFile here: Home Manager's themeFile is for
+        # pkgs.kitty-themes, not local custom theme files.
+        #
+        # Put this after generated settings so theme colors win over any
+        # accidental color settings elsewhere.
+        extraConfig = lib.mkAfter ''
+          include themes/amber-cathode.conf
+        '';
+
         settings = {
-          # shell = "${getExe pkgs.tmux}";
-          cursor_trail = 5; # Fancy cursor movements (especially in nixvim)
-          cursor_trail_decay = "0.03 0.2"; # Animation speed
-          # cursor_trail_start_threshold = "4";
+          # shell = "${pkgs.lib.getExe pkgs.tmux}";
+
+          cursor_trail = 5;
+          cursor_trail_decay = "0.03 0.2";
           strip_trailing_spaces = "smart";
           macos_option_as_alt = "yes";
           macos_quit_when_last_window_closed = true;
@@ -24,22 +37,27 @@
           mouse_hide_wait = 60;
           update_check_interval = 0;
 
-          ## Tabs
+          # Tabs
           tab_title_template = "{index}";
           active_tab_font_style = "normal";
           inactive_tab_font_style = "normal";
           tab_bar_style = "powerline";
           tab_powerline_style = "round";
-          active_tab_foreground = "#1e1e2e";
-          active_tab_background = "#cba6f7";
-          inactive_tab_foreground = "#bac2de";
-          inactive_tab_background = "#313244";
+
+          # Color values moved into themes/amber-cathode.conf:
+          # active_tab_foreground
+          # active_tab_background
+          # inactive_tab_foreground
+          # inactive_tab_background
         };
+
         # shellIntegration.mode = "no-sudo";
+
         keybindings = {
           "ctrl+alt+n" = "launch --cwd=current";
           "alt+w" = "copy_and_clear_or_interrupt";
           "ctrl+y" = "paste_from_clipboard";
+
           "alt+1" = "goto_tab 1";
           "alt+2" = "goto_tab 2";
           "alt+3" = "goto_tab 3";
@@ -54,6 +72,7 @@
           # Tmux
           "ctrl+t" = "launch --cwd=current --type=overlay tmux-sessionizer";
           # "ctrl+t" = "launch --cwd=current --title tmux-sessionizer tmux-sessionizer";
+
           "ctrl+shift+left" = "no_op";
           "ctrl+shift+right" = "no_op";
         };
