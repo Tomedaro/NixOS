74a364c3cbf9a5d12de1371ec90fde6155ff8b96
Tomedaro <daniil.tomenko.r@gmail.com>
2026-06-12 13:48:59 +0200

    chore(nvchad): enable python LSP, remove stale comments

--- modules/programs/editor/nvchad/default.nix
+++ modules/programs/editor/nvchad/default.nix
@@ -19,17 +19,12 @@
         '';
         extraPackages = with pkgs; [
           nixd
-          # nodePackages.bash-language-server
-          # docker-compose-language-service
-          # dockerfile-language-server-nodejs
-          # emmet-language-server
-          /*
-             (python3.withPackages (ps:
-            with ps; [
+          (python3.withPackages (
+            ps: with ps; [
               python-lsp-server
               flake8
-            ]))
-          */
+            ]
+          ))
         ];
         hm-activation = true;
         backup = false;
